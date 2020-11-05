c9f156c8dbf109feac5aa70f99da00ca3c834c4e
KirillTsvetkov <56355589+KirillTsvetkov@users.noreply.github.com>
2020-11-05 19:39:24 +0300

    push

---
+++ TestProject/Models/Contacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProject.Models
+{
+    public partial class Contacts
+    {
+        public Contacts()
+        {
+            Mssg = new HashSet<Mssg>();
+        }
+
+        public int ContaccsId { get; set; }
+        public string ContactsName { get; set; }
+        public string ContactsTel { get; set; }
+        public string ContactsEmail { get; set; }
+
+        public virtual ICollection<Mssg> Mssg { get; set; }
+    }
+}
---
+++ TestProject/Models/Topics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProject.Models
+{
+    public partial class Topics
+    {
+        public Topics()
+        {
+            Mssg = new HashSet<Mssg>();
+        }
+
+        public int TopicsId { get; set; }
+        public string TopicsTitle { get; set; }
+
+        public virtual ICollection<Mssg> Mssg { get; set; }
+    }
+}
